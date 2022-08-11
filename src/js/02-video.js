00ef85b291925ac5e89958cf15dd1335f5dd88ef
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-08-11 21:31:48 +0300

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,82 @@
+import Player from '@vimeo/player';
 
+let throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+//------------------
+// function onPlayUpdateTime(event) {
+//     throttle((event) => { localStorage.setItem("videoplayer-current-time", event.seconds), console.log("Scroll handler call every 1000ms") }, 1000);
+// }
+// player.on('timeupdate', onPlayUpdateTime)
+//-----------------------
+
+function onPlayUpdateTime(event) {
+    localStorage.setItem("videoplayer-current-time", event.seconds)
+}
+
+player.on('timeupdate',
+    throttle((event) => {
+        onPlayUpdateTime
+    }, 1000));
+
+
+
+
+player.getVideoTitle().then(function (title) {
+    console.log('title:', title);
+});
+
+//Забираємо дані з локального схрону
+const timeForSet = localStorage.getItem("videoplayer-current-time");
+
+console.log(Number(timeForSet));
+
+player.setCurrentTime(Number(timeForSet)).then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            timeForSet = 0;
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
+player.on('play', function () {
+    console.log('played the video!');
+});
+
+
+
+/*
+<iframe 
+id???
+src="https://player.vimeo.com/video/76979871?h=8272103f6e" 
+width="640"
+ height="360" 
+ frameborder="0" 
+ allowfullscreen
+  allow="autoplay; 
+ encrypted-media"></iframe>
+
+<script src="https://player.vimeo.com/api/player.js"></script>
+<script>
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+</script>
+
+*/
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,57 @@
+let throttle = require('lodash.throttle');
 
+const emailInputValue = document.querySelector('input');
+const messageInputValue = document.querySelector('textarea');
+const formData = document.querySelector('form');
+
+emailInputValue.addEventListener('input', throttle(onInputSave, 500));
+
+messageInputValue.addEventListener('input', throttle(onInputSave, 500));
+
+formData.addEventListener('submit', (event) => {
+    event.preventDefault();
+    localStorage.removeItem("feedback-form-state");
+    return console.log(setdataForStorage);
+});
+
+
+let getdataFromStorage = localStorage.getItem("feedback-form-state");
+
+let setdataForStorage = {
+    email: getdataFromStorage?.email,
+    message: getdataFromStorage?.message,
+};
+
+//перевод JSON строки з пам'яті в об'єкт в скрипті
+let dataFromStorage = JSON.parse(getdataFromStorage);
+//Значення з пам'яті до субміта
+messageInputValue.value = dataFromStorage?.message ?? ' ';
+emailInputValue.value = dataFromStorage?.email ?? ' ';
+
+function onInputSave(event) {
+    if (this.name === 'email') {
+        setdataForStorage.email = this.value
+    }
+    if (this.name === 'message') {
+        setdataForStorage.message = this.value;
+    }
+    localStorage.setItem("feedback-form-state", JSON.stringify(setdataForStorage));
+}
+
+
+
+/*
+
+
+Отслеживай на форме событие input, и каждый раз записывай в локальное хранилище объект с полями email и message,
+ в которых сохраняй текущие значения полей формы. Пусть ключом для хранилища будет строка "feedback-form-state".
+
+При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные данные, заполняй ими поля формы. 
+В противном случае поля должны быть пустыми.
+
+При сабмите формы очищай хранилище и поля формы, а также выводи объект с полями email, message 
+и текущими их значениями в консоль.
+
+Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. 
+Для этого добавь в проект и используй библиотеку lodash.throttle.
+*/
