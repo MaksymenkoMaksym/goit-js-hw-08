92685bc62b547876684ba0520bfec27a664171d0
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-08-14 18:55:24 +0300

    sacs

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,37 +1,43 @@
 import throttle from 'lodash.throttle';
+import storage from './service-for-local.js'
+
 const emailInputValue = document.querySelector('input');
 const messageInputValue = document.querySelector('textarea');
 const formData = document.querySelector('form');
+const STORAGE_KEY = "feedback-form-state";
+let dataFromForm = {};
 
 emailInputValue.addEventListener('input', throttle(onInputSave, 500));
-
 messageInputValue.addEventListener('input', throttle(onInputSave, 500));
+formData.addEventListener('submit', onSubmitreset);
 
-formData.addEventListener('submit', (event) => {
-    event.preventDefault();
-    localStorage.removeItem("feedback-form-state");
-    return console.log(setdataForStorage);
-});
+let savedData = JSON.parse(localStorage.getItem("STORAGE_KEY"));
 
-let getdataFromStorage = localStorage.getItem("feedback-form-state");
+try {
+    messageInputValue.value = savedData?.message ?? ' ';
+    emailInputValue.value = savedData?.email ?? ' ';
 
-let setdataForStorage = {
-    email: getdataFromStorage?.email,
-    message: getdataFromStorage?.message,
-};
-
-//перевод JSON строки з пам'яті в об'єкт в скрипті
-let dataFromStorage = JSON.parse(getdataFromStorage);
-//Значення з пам'яті до субміта
-messageInputValue.value = dataFromStorage?.message ?? ' ';
-emailInputValue.value = dataFromStorage?.email ?? ' ';
-
-function onInputSave(event) {
-    if (this.name === 'email') {
-        setdataForStorage.email = this.value
-    }
-    if (this.name === 'message') {
-        setdataForStorage.message = this.value;
-    }
-    localStorage.setItem("feedback-form-state", JSON.stringify(setdataForStorage));
+} catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+    console.log(error.stack);
 }
+
+
+function onInputSave() {
+    dataFromForm[this.name] = this.value;
+    localStorage.setItem("STORAGE_KEY", JSON.stringify(dataFromForm));
+}
+
+function onSubmitreset(event) {
+    event.preventDefault();
+
+    if (!messageInputValue.value || !emailInputValue.value) {
+        return
+    }
+
+    storage.remove("STORAGE_KEY");
+    formData.reset();
+    return console.log(dataFromForm);
+}
+
